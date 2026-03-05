d2f7dd2799d6b1e6f63ad7717ede8a29d292af67
FahadRamay <100350062+FahadRamay@users.noreply.github.com>
2026-03-05 09:42:40 +0500

    Add files via upload

---
+++ 4.0 chat bot/responses_dict.py
@@ -0,0 +1,10 @@
+
+
+
+responses = {
+    "what is the capital of pakistan?": "The capital of Pakistan is Islamabad.",
+    "what is the capital of france?": "The capital of France is Paris.",
+    "who is the best teacher?": "The great Arslan Sarwar is the best teacher.",
+    "what is your favourite color": " Blue.",
+    
+}
